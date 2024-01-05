123c52a8d40bb3d991b32e5c79de9ce6308f7e80
Abdirahman Haji <abdirahman.haji.13@gmail.com>
2024-01-05 14:26:09 -0600

    Add logo and update header styling

--- client/components/Header/index.tsx
+++ client/components/Header/index.tsx
@@ -1,9 +1,25 @@
+import Image from 'next/image';
 import Link from 'next/link';
+import { Montserrat } from 'next/font/google';
+
+const montserrat = Montserrat({
+  subsets: ['latin'],
+});
 
 const Header = () => {
   return (
-    <header className="w-full flex flex-row justify-between py-8 border border-purple-500 pl-14 pr-24">
-      <section>Logo</section>
+    <header className="w-full flex flex-row justify-between items-center py-4 border border-purple-500 pl-14 pr-24">
+      <section className="items-center flex flex-row gap-1">
+        <Image src="/logo.png" alt="Furniro" width={50} height={32} />
+        <p
+          className="text-2xl font-bold"
+          style={{
+            fontFamily: montserrat.style.fontFamily,
+          }}
+        >
+          Furniro
+        </p>
+      </section>
 
       <nav>
         <ul className="flex flex-row gap-16">
