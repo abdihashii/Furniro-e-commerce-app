568c08b3b94bdecdc55e485f34b2414731dcae53
Abdirahman Haji <abdirahman.haji.13@gmail.com>
2024-01-05 19:00:43 -0600

    Add a bg hover overlay on products

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,3 +1,6 @@
+import Compare from '@/components/Icons/Compare';
+import Heart from '@/components/Icons/Heart';
+import Share from '@/components/Icons/Share';
 import Image from 'next/image';
 
 export default function Home() {
@@ -81,27 +84,76 @@ export default function Home() {
         <h2 className="text-[40px] font-bold text-center">Our Products</h2>
 
         <article className="grid-cols-4 grid gap-8">
-          <div className="h-[446px] bg-[#F4F5F7] flex flex-col group">
-            <div className="grow relative">
+          <div className="h-[446px] bg-[#F4F5F7] flex flex-col group relative">
+            {/* Overlay */}
+            <div className="absolute inset-0 bg-black bg-opacity-0 opacity-0 group-hover:bg-opacity-50 group-hover:opacity-100 flex justify-center items-center transition-opacity ease-linear duration-200 z-10">
+              {/* Overlay content */}
+              <div className="text-center font-semibold space-y-6">
+                <button className="bg-white text-[#B88E2F] px-12 py-3 hover:bg-[#B88E2F] hover:text-white transition-colors ease-linear duration-100">
+                  Add to cart
+                </button>
+
+                <div className="flex flex-row gap-5">
+                  {/* Other elements can go here */}
+                  <button className="text-white flex flex-row items-center gap-1 hover:underline">
+                    <Share /> Share
+                  </button>
+                  <button className="text-white flex flex-row items-center gap-1 hover:underline">
+                    <Compare /> Compare
+                  </button>
+                  <button className="text-white flex flex-row items-center gap-1 hover:underline">
+                    <Heart />
+                    Like
+                  </button>
+                </div>
+              </div>
+            </div>
+
+            {/* Image */}
+            <div className="grow relative z-0">
               <Image
                 src="/product-1.png"
                 alt="Product 1"
-                fill={true}
+                layout="fill"
                 objectFit="cover"
                 objectPosition="bottom"
               />
             </div>
 
-            <div className="space-y-2 px-4 pt-4 pb-8 item">
-              <h3 className="text-2xl font-semibold text-[#3A3A3A]">
-                Syltherine
-              </h3>
-              <p className="font-medium text-[#898989]">Stylish cafe chair</p>
-              <p className="text-xl font-semibold text-[#3A3A3A]">$200</p>
+            {/* Item data */}
+            <div className="space-y-2 px-4 pt-4 pb-8 z-0">
+              <h3 className="text-2xl font-semibold text-[#3A3A3A]">Pingky</h3>
+              <p className="font-medium text-[#898989]">Sofa</p>
+              <p className="text-xl font-semibold text-[#3A3A3A]">$900</p>
             </div>
           </div>
 
-          <div className="h-[446px] bg-[#F4F5F7] flex flex-col">
+          <div className="h-[446px] bg-[#F4F5F7] flex flex-col group relative">
+            {/* Overlay */}
+            <div className="absolute inset-0 bg-black bg-opacity-0 opacity-0 group-hover:bg-opacity-50 group-hover:opacity-100 flex justify-center items-center transition-opacity ease-linear duration-200 z-10">
+              {/* Overlay content */}
+              <div className="text-center font-semibold space-y-6">
+                <button className="bg-white text-[#B88E2F] px-12 py-3 hover:bg-[#B88E2F] hover:text-white transition-colors ease-linear duration-100">
+                  Add to cart
+                </button>
+
+                <div className="flex flex-row gap-5">
+                  {/* Other elements can go here */}
+                  <button className="text-white flex flex-row items-center gap-1 hover:underline">
+                    <Share /> Share
+                  </button>
+                  <button className="text-white flex flex-row items-center gap-1 hover:underline">
+                    <Compare /> Compare
+                  </button>
+                  <button className="text-white flex flex-row items-center gap-1 hover:underline">
+                    <Heart />
+                    Like
+                  </button>
+                </div>
+              </div>
+            </div>
+
+            {/* Image */}
             <div className="grow relative">
               <Image
                 src="/product-1.png"
@@ -119,7 +171,32 @@ export default function Home() {
             </div>
           </div>
 
-          <div className="h-[446px] bg-[#F4F5F7] flex flex-col">
+          <div className="h-[446px] bg-[#F4F5F7] flex flex-col group relative">
+            {/* Overlay */}
+            <div className="absolute inset-0 bg-black bg-opacity-0 opacity-0 group-hover:bg-opacity-50 group-hover:opacity-100 flex justify-center items-center transition-opacity ease-linear duration-200 z-10">
+              {/* Overlay content */}
+              <div className="text-center font-semibold space-y-6">
+                <button className="bg-white text-[#B88E2F] px-12 py-3 hover:bg-[#B88E2F] hover:text-white transition-colors ease-linear duration-100">
+                  Add to cart
+                </button>
+
+                <div className="flex flex-row gap-5">
+                  {/* Other elements can go here */}
+                  <button className="text-white flex flex-row items-center gap-1 hover:underline">
+                    <Share /> Share
+                  </button>
+                  <button className="text-white flex flex-row items-center gap-1 hover:underline">
+                    <Compare /> Compare
+                  </button>
+                  <button className="text-white flex flex-row items-center gap-1 hover:underline">
+                    <Heart />
+                    Like
+                  </button>
+                </div>
+              </div>
+            </div>
+
+            {/* Image */}
             <div className="grow relative">
               <Image
                 src="/product-1.png"
@@ -137,7 +214,32 @@ export default function Home() {
             </div>
           </div>
 
-          <div className="h-[446px] bg-[#F4F5F7] flex flex-col">
+          <div className="h-[446px] bg-[#F4F5F7] flex flex-col group relative">
+            {/* Overlay */}
+            <div className="absolute inset-0 bg-black bg-opacity-0 opacity-0 group-hover:bg-opacity-50 group-hover:opacity-100 flex justify-center items-center transition-opacity ease-linear duration-200 z-10">
+              {/* Overlay content */}
+              <div className="text-center font-semibold space-y-6">
+                <button className="bg-white text-[#B88E2F] px-12 py-3 hover:bg-[#B88E2F] hover:text-white transition-colors ease-linear duration-100">
+                  Add to cart
+                </button>
+
+                <div className="flex flex-row gap-5">
+                  {/* Other elements can go here */}
+                  <button className="text-white flex flex-row items-center gap-1 hover:underline">
+                    <Share /> Share
+                  </button>
+                  <button className="text-white flex flex-row items-center gap-1 hover:underline">
+                    <Compare /> Compare
+                  </button>
+                  <button className="text-white flex flex-row items-center gap-1 hover:underline">
+                    <Heart />
+                    Like
+                  </button>
+                </div>
+              </div>
+            </div>
+
+            {/* Image */}
             <div className="grow relative">
               <Image
                 src="/product-1.png"
@@ -157,7 +259,32 @@ export default function Home() {
             </div>
           </div>
 
-          <div className="h-[446px] bg-[#F4F5F7] flex flex-col">
+          <div className="h-[446px] bg-[#F4F5F7] flex flex-col group relative">
+            {/* Overlay */}
+            <div className="absolute inset-0 bg-black bg-opacity-0 opacity-0 group-hover:bg-opacity-50 group-hover:opacity-100 flex justify-center items-center transition-opacity ease-linear duration-200 z-10">
+              {/* Overlay content */}
+              <div className="text-center font-semibold space-y-6">
+                <button className="bg-white text-[#B88E2F] px-12 py-3 hover:bg-[#B88E2F] hover:text-white transition-colors ease-linear duration-100">
+                  Add to cart
+                </button>
+
+                <div className="flex flex-row gap-5">
+                  {/* Other elements can go here */}
+                  <button className="text-white flex flex-row items-center gap-1 hover:underline">
+                    <Share /> Share
+                  </button>
+                  <button className="text-white flex flex-row items-center gap-1 hover:underline">
+                    <Compare /> Compare
+                  </button>
+                  <button className="text-white flex flex-row items-center gap-1 hover:underline">
+                    <Heart />
+                    Like
+                  </button>
+                </div>
+              </div>
+            </div>
+
+            {/* Image */}
             <div className="grow relative">
               <Image
                 src="/product-1.png"
@@ -175,7 +302,32 @@ export default function Home() {
             </div>
           </div>
 
-          <div className="h-[446px] bg-[#F4F5F7] flex flex-col">
+          <div className="h-[446px] bg-[#F4F5F7] flex flex-col group relative">
+            {/* Overlay */}
+            <div className="absolute inset-0 bg-black bg-opacity-0 opacity-0 group-hover:bg-opacity-50 group-hover:opacity-100 flex justify-center items-center transition-opacity ease-linear duration-200 z-10">
+              {/* Overlay content */}
+              <div className="text-center font-semibold space-y-6">
+                <button className="bg-white text-[#B88E2F] px-12 py-3 hover:bg-[#B88E2F] hover:text-white transition-colors ease-linear duration-100">
+                  Add to cart
+                </button>
+
+                <div className="flex flex-row gap-5">
+                  {/* Other elements can go here */}
+                  <button className="text-white flex flex-row items-center gap-1 hover:underline">
+                    <Share /> Share
+                  </button>
+                  <button className="text-white flex flex-row items-center gap-1 hover:underline">
+                    <Compare /> Compare
+                  </button>
+                  <button className="text-white flex flex-row items-center gap-1 hover:underline">
+                    <Heart />
+                    Like
+                  </button>
+                </div>
+              </div>
+            </div>
+
+            {/* Image */}
             <div className="grow relative">
               <Image
                 src="/product-1.png"
@@ -193,7 +345,32 @@ export default function Home() {
             </div>
           </div>
 
-          <div className="h-[446px] bg-[#F4F5F7] flex flex-col">
+          <div className="h-[446px] bg-[#F4F5F7] flex flex-col group relative">
+            {/* Overlay */}
+            <div className="absolute inset-0 bg-black bg-opacity-0 opacity-0 group-hover:bg-opacity-50 group-hover:opacity-100 flex justify-center items-center transition-opacity ease-linear duration-200 z-10">
+              {/* Overlay content */}
+              <div className="text-center font-semibold space-y-6">
+                <button className="bg-white text-[#B88E2F] px-12 py-3 hover:bg-[#B88E2F] hover:text-white transition-colors ease-linear duration-100">
+                  Add to cart
+                </button>
+
+                <div className="flex flex-row gap-5">
+                  {/* Other elements can go here */}
+                  <button className="text-white flex flex-row items-center gap-1 hover:underline">
+                    <Share /> Share
+                  </button>
+                  <button className="text-white flex flex-row items-center gap-1 hover:underline">
+                    <Compare /> Compare
+                  </button>
+                  <button className="text-white flex flex-row items-center gap-1 hover:underline">
+                    <Heart />
+                    Like
+                  </button>
+                </div>
+              </div>
+            </div>
+
+            {/* Image */}
             <div className="grow relative">
               <Image
                 src="/product-1.png"
@@ -211,7 +388,32 @@ export default function Home() {
             </div>
           </div>
 
-          <div className="h-[446px] bg-[#F4F5F7] flex flex-col">
+          <div className="h-[446px] bg-[#F4F5F7] flex flex-col group relative">
+            {/* Overlay */}
+            <div className="absolute inset-0 bg-black bg-opacity-0 opacity-0 group-hover:bg-opacity-50 group-hover:opacity-100 flex justify-center items-center transition-opacity ease-linear duration-200 z-10">
+              {/* Overlay content */}
+              <div className="text-center font-semibold space-y-6">
+                <button className="bg-white text-[#B88E2F] px-12 py-3 hover:bg-[#B88E2F] hover:text-white transition-colors ease-linear duration-100">
+                  Add to cart
+                </button>
+
+                <div className="flex flex-row gap-5">
+                  {/* Other elements can go here */}
+                  <button className="text-white flex flex-row items-center gap-1 hover:underline">
+                    <Share /> Share
+                  </button>
+                  <button className="text-white flex flex-row items-center gap-1 hover:underline">
+                    <Compare /> Compare
+                  </button>
+                  <button className="text-white flex flex-row items-center gap-1 hover:underline">
+                    <Heart />
+                    Like
+                  </button>
+                </div>
+              </div>
+            </div>
+
+            {/* Image */}
             <div className="grow relative">
               <Image
                 src="/product-1.png"
@@ -230,6 +432,7 @@ export default function Home() {
           </div>
         </article>
 
+        {/* Show more button */}
         <article className="flex justify-center">
           <button className="border text-[#B88E2F] font-semibold border-[#B88E2F] py-3 px-20 hover:underline">
             Show More
