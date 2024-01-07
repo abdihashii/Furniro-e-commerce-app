84256965511928ff9b66f28e40a9f6dc90876348
Abdirahman Haji <abdirahman.haji.13@gmail.com>
2024-01-07 14:00:42 -0600

    Add new icons: AccountAlert, Heart, and Search

--- client/components/Icons/Heart.tsx
+++ client/components/Icons/Heart.tsx
@@ -1,18 +1,32 @@
 import React from 'react';
 
-const Heart = () => {
+const Heart = ({
+  className,
+  stroke,
+  width,
+  height,
+}: {
+  className?: string;
+  stroke?: {
+    color?: string;
+    width?: number;
+  };
+  width?: number;
+  height?: number;
+}) => {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      width="16"
-      height="16"
+      width={width || '16'}
+      height={height || '16'}
       viewBox="0 0 16 16"
       fill="none"
+      className={className}
     >
       <path
         d="M7.99973 14.0361C-5.33333 6.66669 3.99999 -1.33331 7.99973 3.72539C12 -1.33331 21.3333 6.66669 7.99973 14.0361Z"
-        stroke="white"
-        stroke-width="1.8"
+        stroke={stroke?.color || 'white'}
+        stroke-width={stroke?.width || '1.8'}
       />
     </svg>
   );
