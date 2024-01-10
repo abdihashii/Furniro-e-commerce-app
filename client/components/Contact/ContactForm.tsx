6f86f8b152dca5e1b2985cb6d9daeb2d4e356278
Abdirahman Haji <abdirahman.haji.13@gmail.com>
2024-01-09 22:16:10 -0600

    Refactor ContactForm component to handle form validation

--- client/components/Contact/ContactForm.tsx
+++ client/components/Contact/ContactForm.tsx
@@ -1,48 +1,89 @@
+'use client';
+
 import React from 'react';
+import { FieldValues, useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { contactFormSchema } from '@/schemas';
 
 const ContactForm = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: zodResolver(contactFormSchema),
+  });
+
+  const onSubmit = (data: FieldValues) => {
+    alert(JSON.stringify(data, null, 2));
+  };
+
   return (
-    <form className="space-y-9 w-1/2">
-      <div className="flex flex-col gap-5">
-        <label htmlFor="name">Your name</label>
+    <form className="space-y-6 w-1/2" onSubmit={handleSubmit(onSubmit)}>
+      {/* Name */}
+      <div className="flex flex-col gap-2">
+        <label htmlFor="name">* Your name</label>
         <input
           className="border border-[#9F9F9F] px-6 py-4 rounded-lg"
-          type="text"
-          id="name"
           placeholder="John Doe"
+          {...register('name')}
         />
+        {errors.name ? (
+          <p className="text-red-500">{errors.name.message as string}</p>
+        ) : (
+          <p className="text-red-500">&nbsp;</p>
+        )}
       </div>
 
-      <div className="flex flex-col gap-5">
-        <label htmlFor="email">Your email</label>
+      {/* Email */}
+      <div className="flex flex-col gap-2">
+        <label htmlFor="email">* Your email</label>
         <input
           className="border border-[#9F9F9F] px-6 py-4 rounded-lg"
-          type="email"
-          id="email"
           placeholder="john.doe@gmail.com"
+          {...register('email')}
         />
+        {errors.email ? (
+          <p className="text-red-500">{errors.email.message as string}</p>
+        ) : (
+          <p className="text-red-500">&nbsp;</p>
+        )}
       </div>
 
-      <div className="flex flex-col gap-5">
+      {/* Subject */}
+      <div className="flex flex-col gap-2">
         <label htmlFor="subject">Subject</label>
         <input
           className="border border-[#9F9F9F] px-6 py-4 rounded-lg"
-          type="text"
-          id="subject"
           placeholder='e.g. "Product Inquiry"'
+          {...register('subject')}
         />
+        {errors.subject ? (
+          <p className="text-red-500">{errors.subject.message as string}</p>
+        ) : (
+          <p className="text-red-500">&nbsp;</p>
+        )}
       </div>
 
-      <div className="flex flex-col gap-5">
-        <label htmlFor="message">Your message</label>
+      {/* Message */}
+      <div className="flex flex-col gap-2">
+        <label htmlFor="message">* Your message</label>
         <textarea
           className="border border-[#9F9F9F] px-6 py-4 rounded-lg"
-          id="message"
           placeholder="Hi! I'd like to ask about"
+          {...register('message')}
         />
+        {errors.message ? (
+          <p className="text-red-500">{errors.message.message as string}</p>
+        ) : (
+          <p className="text-red-500">&nbsp;</p>
+        )}
       </div>
 
-      <button className="bg-[#B88E2F] border border-[#B88E2F] text-white px-24 py-4 rounded-md hover:text-[#B88E2F] hover:bg-white">
+      <button
+        className="bg-[#B88E2F] border border-[#B88E2F] text-white px-24 py-4 rounded-md hover:text-[#B88E2F] hover:bg-white"
+        type="submit"
+      >
         Submit
       </button>
     </form>
