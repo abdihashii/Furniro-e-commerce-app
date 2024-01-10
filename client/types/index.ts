216c196953df241f3266eadc6f1b7ec9005b8c55
Abdirahman Haji <abdirahman.haji.13@gmail.com>
2024-01-10 14:52:48 -0600

    Add IProduct interface to client types

---
+++ client/types/index.ts
@@ -0,0 +1,10 @@
+export interface IProduct {
+	created_at: string;
+	description: string | null;
+	id: string;
+	img_src: string | null;
+	name: string;
+	price: number;
+	slug: string;
+	updated_at: string | null;
+}
