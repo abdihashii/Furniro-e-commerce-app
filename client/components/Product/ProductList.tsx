6bcad9ab35d97cc81cd34894ab3897506093b80d
Abdirahman Haji <abdirahman.haji.13@gmail.com>
2024-01-12 17:35:01 -0600

    on our way to good pagination!

--- client/components/Product/ProductList.tsx
+++ client/components/Product/ProductList.tsx
@@ -1,41 +1,97 @@
 'use client';
 
+import { IProduct } from '@/types';
+import { Database } from '@/types/database.types';
+import { createClientComponentClient } from '@supabase/auth-helpers-nextjs';
 import React, { useEffect } from 'react';
+
+import Product from '.';
+import Filter from '../Icons/Filter';
 import ListLayout1 from '../Icons/ListLayout1';
 import ListLayout2 from '../Icons/ListLayout2';
-import Filter from '../Icons/Filter';
-import { createClientComponentClient } from '@supabase/auth-helpers-nextjs';
-import { Database } from '@/types/database.types';
-import { IProduct } from '@/types';
-import Product from '.';
 
 const ProductList = () => {
 	const supabase = createClientComponentClient<Database>();
 
+	// Product states
 	const [products, setProducts] = React.useState<IProduct[]>([]);
 
+	// Pagination states
+	const [pageNum, setPageNum] = React.useState<number>(1); // Current page number
+	const [numOfPages, setNumOfPages] = React.useState<number | null>(null); // Number of pages in the pagination
+
+	// Filter states
+	const [pageSize, setPageSize] = React.useState<number>(12); // Number of products per page
+	const [sortBy, setSortBy] = React.useState<string>('price');
+
+	// Loading state
+	const [isLoading, setIsLoading] = React.useState<boolean>(false);
+
+	const fetchProducts = async (pageNum: number, pageSize: number) => {
+		try {
+			setIsLoading(true);
+
+			// Reset products and number of pages state to initial values
+			setProducts([]);
+			setNumOfPages(null);
+
+			const { data, error } = await supabase
+				.from('products')
+				.select('*')
+				.order('created_at', { ascending: false })
+				.range((pageNum - 1) * pageSize, pageNum * pageSize - 1);
+
+			if (error) {
+				throw error;
+			}
+
+			setProducts(data);
+		} catch (error) {
+			console.log(error);
+		} finally {
+			setIsLoading(false);
+		}
+	};
+
+	// Fetch products on page load and when page number changes
 	useEffect(() => {
-		const fetchProducts = async () => {
+		fetchProducts(pageNum, pageSize);
+	}, [pageNum]);
+
+	// Calculate number of pages
+	useEffect(() => {
+		const fetchNumOfPages = async () => {
 			try {
-				const { data, error } = await supabase.from('products').select('*');
+				const { count } = await supabase
+					.from('products')
+					.select('*', { count: 'exact' });
 
-				if (error) {
-					throw error;
-				}
+				if (count === null) throw new Error('Count is null');
 
-				setProducts(data);
+				setNumOfPages(Math.ceil(count / pageSize));
 			} catch (error) {
 				console.log(error);
 			}
 		};
 
-		fetchProducts();
-	}, []);
+		fetchNumOfPages();
+	}, [pageSize]);
+
+	// Loading indicator
+	const renderLoadingIndicator = () => {
+		if (!isLoading) return null;
+
+		return (
+			<div className="flex h-full w-full flex-row items-center justify-center">
+				<div className="h-32 w-32 animate-spin rounded-full border-b-2 border-t-2 border-gray-900"></div>
+			</div>
+		);
+	};
 
 	return (
 		<>
 			{/* Filter section */}
-			<section className="flex flex-row justify-between bg-[#F9F1E7] px-[200px] py-6">
+			<section className="flex flex-row justify-between bg-[#F9F1E7] py-6 lg:px-[100px] 2xl:px-[200px]">
 				<article className="flex flex-row items-center gap-6">
 					<div className="flex flex-row items-center gap-3">
 						<Filter /> <p className="text-xl">Filter</p>
@@ -50,10 +106,17 @@ const ProductList = () => {
 					<div className="flex flex-row items-center gap-4 text-xl">
 						<p>Show</p>
 
-						<select className="appearance-none px-4 py-3 text-[#9F9F9F]">
-							<option>16</option>
-							<option>32</option>
-							<option>48</option>
+						<select
+							className="appearance-none px-4 py-3 text-[#9F9F9F]"
+							onChange={(e) => {
+								setPageSize(Number(e.target.value));
+								fetchProducts(pageNum, Number(e.target.value));
+							}}
+							value={pageSize}
+						>
+							<option>12</option>
+							<option>24</option>
+							<option>36</option>
 						</select>
 					</div>
 
@@ -69,9 +132,11 @@ const ProductList = () => {
 				</article>
 			</section>
 
-			{/* Products list section */}
-			<section className="flex flex-col items-center gap-16 px-[200px] pb-20 pt-16">
-				<article className="grid w-full gap-x-8 gap-y-10 md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-5">
+			{/* Product section */}
+			<section className="flex flex-col items-center gap-16 pb-20 pt-16 lg:px-[100px] 2xl:px-[200px]">
+				{renderLoadingIndicator()}
+
+				<article className="grid w-full gap-x-8 gap-y-10 md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4">
 					{products.map((product, index) => {
 						const cleanSrc = product.img_src?.replace(/^"|"$/g, '');
 
@@ -91,21 +156,27 @@ const ProductList = () => {
 				</article>
 
 				<article className="flex flex-row gap-9">
-					<button className="gap-9 rounded-xl bg-[#B88E2F] px-6 py-4 text-white">
+					{numOfPages &&
+						Array.from({ length: numOfPages }, (_, i) => (
+							<button
+								key={i}
+								className={`${
+									pageNum === i + 1 ? 'bg-[#B88E2F] text-white' : 'bg-[#F9F1E7]'
+								} gap-9 rounded-xl px-6 py-4 hover:bg-[#B88E2F] hover:text-white`}
+							>
+								{i + 1}
+							</button>
+						))}
+
+					{/* <button className="gap-9 rounded-xl bg-[#B88E2F] px-6 py-4 text-white">
 						1
-					</button>
+					</button> */}
 
-					<button className="gap-9 rounded-xl bg-[#F9F1E7] px-6 py-4 hover:bg-[#B88E2F] hover:text-white">
-						2
-					</button>
-
-					<button className="gap-9 rounded-xl bg-[#F9F1E7] px-6 py-4 hover:bg-[#B88E2F] hover:text-white">
-						3
-					</button>
-
-					<button className="gap-9 rounded-xl bg-[#F9F1E7] px-6 py-4 hover:bg-[#B88E2F] hover:text-white">
-						Next
-					</button>
+					{numOfPages && numOfPages > 3 && (
+						<button className="gap-9 rounded-xl bg-[#F9F1E7] px-6 py-4 hover:bg-[#B88E2F] hover:text-white">
+							Next
+						</button>
+					)}
 				</article>
 			</section>
 		</>
