ad7afd5d73ccfad189ddd630f9524e2d317dcf2c
Abdirahman Haji <abdirahman.haji.13@gmail.com>
2024-01-14 12:51:54 -0600

    Refactor OurProductsSection component

--- client/components/Home/OurProductsSection.tsx
+++ client/components/Home/OurProductsSection.tsx
@@ -5,51 +5,51 @@ import { createClientComponentClient } from '@supabase/auth-helpers-nextjs';
 import { Database } from '@/types/database.types';
 
 const OurProductsSection = async () => {
-  const supabase = createClientComponentClient<Database>();
+	const supabase = createClientComponentClient<Database>();
 
-  const { data: products, error } = await supabase
-    .from('products')
-    .select('*')
-    .order('created_at', { ascending: false })
-    .limit(4);
+	const { data: products, error } = await supabase
+		.from('products')
+		.select('*')
+		.order('created_at', { ascending: false })
+		.limit(4);
 
-  if (error) {
-    console.log(error);
+	if (error) {
+		console.log(error);
 
-    return <div>Error loading products</div>;
-  }
+		return <div>Error loading products</div>;
+	}
 
-  return (
-    <section className="w-full space-y-8 py-14 lg:px-[200px] px-20">
-      <h2 className="text-[40px] font-bold text-center">Our Products</h2>
+	return (
+		<section className="w-full space-y-8 px-20 py-14 lg:px-[200px]">
+			<h2 className="text-center text-[40px] font-bold">Our Products</h2>
 
-      <article className="md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 grid gap-8 grid-cols-1">
-        {products?.map((product) => {
-          const cleanSrc = product.img_src?.replace(/^"|"$/g, '');
+			<article className="grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+				{products?.map((product) => {
+					const cleanSrc = product.img_src?.replace(/^"|"$/g, '');
 
-          return (
-            <Product
-              key={product.id}
-              productName={product.name}
-              productDescription={product.description ?? ''}
-              productPrice={`$${product.price}`}
-              productImg={{ src: cleanSrc ?? '', alt: product.name }}
-            />
-          );
-        })}
-      </article>
+					return (
+						<Product
+							key={product.id}
+							productName={product.name}
+							productDescription={product.description ?? ''}
+							productPrice={`$${product.price}`}
+							productImg={{ src: cleanSrc ?? '', alt: product.name }}
+						/>
+					);
+				})}
+			</article>
 
-      {/* Show more button */}
-      <article className="flex justify-center">
-        <Link
-          href="/shop"
-          className="border text-[#B88E2F] font-semibold border-[#B88E2F] py-3 px-20 hover:text-white hover:bg-[#B88E2F]"
-        >
-          Show More
-        </Link>
-      </article>
-    </section>
-  );
+			{/* Show more button */}
+			<article className="flex justify-center">
+				<Link
+					href="/shop"
+					className="border border-[#B88E2F] px-20 py-3 font-semibold text-[#B88E2F] hover:bg-[#B88E2F] hover:text-white"
+				>
+					Show More
+				</Link>
+			</article>
+		</section>
+	);
 };
 
 export default OurProductsSection;
