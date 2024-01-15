85bdf9cd07070d9e564136f3ae3ba33fdec635b9
Abdirahman Haji <abdirahman.haji.13@gmail.com>
2024-01-14 19:40:24 -0600

    Refactor cart items layout and add Trash icon

--- client/components/Cart/CartItems.tsx
+++ client/components/Cart/CartItems.tsx
@@ -8,42 +8,57 @@ const CartItems = () => {
 	const { cart, deleteFromCart } = useCart();
 
 	return (
-		<section className="py-20 lg:px-[100px] xl:px-[200px] 2xl:px-[300px]">
-			<article>
-				{/* Header for the cart items */}
-				<div className="flex flex-row gap-9 bg-[#F9F1E7] py-4 pl-36 font-medium">
-					<p>Product</p>
-					<p>Price</p>
-					<p>Quanity</p>
-				</div>
-
+		<section className="flex w-full flex-row gap-8 py-20 lg:px-[100px] xl:px-[200px] 2xl:px-[300px]">
+			<article className="w-3/4">
 				{/* Cart items */}
-				<ul className="flex flex-col">
-					{cart.map((cartItem) => (
-						<li
-							key={cartItem.id}
-							className="flex flex-row gap-9"
-						>
-							<Image
-								src={cartItem.img_src as string}
-								alt={cartItem.name}
-								width={100}
-								height={100}
-							/>
+				<table className="w-full">
+					<thead className="w-full bg-[#F9F1E7]">
+						<tr className="font-medium">
+							<th className="py-4"></th>
+							<th className="py-4">Product</th>
+							<th className="py-4">Price</th>
+							<th className="py-4">Quantity</th>
+							<th className="py-4"></th>
+						</tr>
+					</thead>
 
-							<p>{cartItem.name}</p>
-							<p>{cartItem.price}</p>
-							<p>{cartItem.quantity ?? 1}</p>
-
-							<button
-								className="rounded-md bg-red-500 px-4 text-white hover:bg-red-700"
-								onClick={() => deleteFromCart(cartItem)}
+					<tbody>
+						{cart.map((cartItem) => (
+							<tr
+								key={cartItem.id}
+								className="text-center"
 							>
-								Delete
-							</button>
-						</li>
-					))}
-				</ul>
+								<td className="pt-8">
+									<Image
+										src={cartItem.img_src as string}
+										alt={cartItem.name}
+										width={100}
+										height={100}
+									/>
+								</td>
+
+								<td className="pt-8">{cartItem.name}</td>
+
+								<td className="pt-8">${cartItem.price}</td>
+
+								<td className="pt-8">{cartItem.quantity ?? 1}</td>
+
+								<td className="pt-8">
+									<button
+										className="rounded-md bg-red-500 px-4 text-white hover:bg-red-700"
+										onClick={() => deleteFromCart(cartItem)}
+									>
+										Delete
+									</button>
+								</td>
+							</tr>
+						))}
+					</tbody>
+				</table>
+			</article>
+
+			<article className="h-[390px] w-1/4 bg-[#F9F1E7]">
+				<h1>Cart Totals</h1>
 			</article>
 		</section>
 	);
